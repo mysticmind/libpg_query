c684e10f86fa2e1ac47f83ecfd279cd7b963cdfc
mysticmind <babu.annamalai@gmail.com>
2025-07-31 07:33:02 +0530

    Changes to dynamically generate pg_query.def

--- gen_def.ps1
+++
@@ -1,8 +0,0 @@
-Get-Content pg_query.h | ForEach-Object {
-    if ($_ -match "pg_query_([a-zA-Z0-9_]+)\s*\(") {
-        $fn = $matches[1]
-        if ($fn -ne "pg_query_init") {
-            "    $fn" >> "pg_query.def"
-        }
-    }
-}
---
+++ generate_def.ps1
@@ -0,0 +1,15 @@
+$functions = Get-Content pg_query.h | ForEach-Object {
+    if ($_ -cmatch '\b(pg_query_\w+)\s*\(') {
+        $fn = $matches[1]
+        if ($fn -ne "pg_query_init") {
+            "    $fn"
+        }
+    }
+}
+
+$output = @(
+    "LIBRARY pg_query"
+    "EXPORTS"
+) + $functions
+
+$output | Set-Content pg_query.def
